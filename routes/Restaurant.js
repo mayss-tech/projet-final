d6ee6c5397be307b565d1f482476a3a2ad778971
mayssa louati <mayssaloau38@gmail.com>
2020-12-11 11:40:11 +0100

    v11

--- routes/Restaurant.js
+++ routes/Restaurant.js
@@ -1,6 +1,5 @@
 const express=require('express');
 const {restaurantList,restaurantFind, restaurantDetails, rating} =require('../controllers/restaurants.controller.js');
-
 const Router=express.Router()
 
 Router.post('/restaurantList', restaurantList);
